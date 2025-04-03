950e9ed9f0cf927f3d88eec4e510e5f93ddddcf0
Vladislav <icvladislav.seden@americanwell.com>
2025-04-03 18:51:48 +0200

    GetResponceObjectModel added in models dir

---
+++ models/GetResponceObjectModel.ts
@@ -0,0 +1,6 @@
+export interface GetResponceObjectModel {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
--- tests/api/users.spec.ts
+++ tests/api/users.spec.ts
@@ -1,10 +1,13 @@
 import { test, expect } from '@playwright/test';
 import { APIHelper } from '../../helpers/api-helpers';
 import { allure } from 'allure-playwright'; 
-
+import { APIEndpoints } from '../../config/api-endpoints';
+import { GetResponceObjectModel } from '../../models/GetResponceObjectModel';
 
 test.describe('Users API', () => {
     let apiHelper: APIHelper;
+    const postsEndpoint = APIEndpoints.POSTS;
+
 
     test.beforeEach(async ({ request }) => {
         apiHelper = new APIHelper(request);
@@ -13,16 +16,19 @@ test.describe('Users API', () => {
     test('should get user details @smoke @regression', async () => {
         // Add allure steps
         await allure.step('Send GET request to /users/1', async () => {
-            const response = await apiHelper.get('/users/1');
+            const response = await apiHelper.get(postsEndpoint);
             
             await allure.step('Verify response status is 200', async () => {
                 expect(response.status()).toBe(200);
             });
 
-            const body = await response.json();
+            const body = await response.json() as GetResponceObjectModel[];
+            console.log(body);
             await allure.step('Verify response body contains required fields', async () => {
-                expect(body).toHaveProperty('id');
-                expect(body).toHaveProperty('name');
+                expect(body[0]).toHaveProperty('userId');
+                expect(body[0]).toHaveProperty('id');
+                expect(body[0]).toHaveProperty('title');
+                expect(body[0]).toHaveProperty('body');
             });
 
             // Attach response to allure report
